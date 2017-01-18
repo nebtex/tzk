2d42c8e00d6aac206d708390d67e3f84ff99afbc
Cristian Lozano <criscalovis@gmail.com>
2017-01-18 18:14:01 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,9 +21,11 @@ apt-get update -y && apt-get install curl wget apt-transport-https -y
 # install sigil
 curl -fsSL https://github.com/gliderlabs/sigil/releases/download/v0.4.0/sigil_0.4.0_Linux_x86_64.tgz | tar -zxC /usr/local/bin
 
+curl -o /tmp/tzk.toml https://raw.githubusercontent.com/NebTex/tzk/master/kubernetes.sh
+
 # install tzk daemon
 RUN wget https://github.com/NebTex/tzk-daemon/releases/download/v${TZKD_VERSION}/tzkd_linux_amd64 && \
-    cp tzkd_linux_amd64 /usr/local/bin/tzkd && \
+    mv tzkd_linux_amd64 /usr/local/bin/tzkd && \
     chmod +x /usr/local/bin/tzkd && \
     mkdir -p /etc/tzk.d
        
@@ -35,7 +37,7 @@ if [ "${master:-false}" == "true" ];then
     chmod 755 -R /caddy
 fi
  
- sigil -p -i "$(cat /templates/tzk.toml)" \
+ sigil -p -i "$(cat /tmp/tzk.toml)" \
     VPNName=${VPNName:-tzk} ACLToken=${ACLToken:?} master=${master:-false} \
     Subnet=${Subnet:-10.187.0.0/16} ConsulHost=${ConsulHost:?} \
     NodeIP=${NodeIP:-} \
